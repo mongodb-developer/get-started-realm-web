88def2acaac981268db5f7909b244118a646951c
Wan Bachtiar <sindbach@gmail.com>
2021-01-29 17:51:03 +1100

    Removed variable app fetching

--- get-setup.sh
+++ get-setup.sh
@@ -22,7 +22,5 @@ docker run --rm \
      git clone --depth 1 https://github.com/sindbach/realm-tutorial-web.git; \
      sed -i 's/\"clusterName\": \"[a-zA-Z0-9]\+\"/\"clusterName\": \"${CLUSTER_NAME}\"/g' realm-tutorial-backend/services/mongodb-atlas/config.json;
      realm-cli login --api-key ${PUBLIC_KEY} --private-api-key ${PRIVATE_KEY}; \
-     realm-cli import --yes --path ./realm-tutorial-backend > realm-import.txt 2>&1; \
-     cat realm-import.txt; \
-     NEWAPP=\`grep -r 'Successfully imported ' realm-import.txt | awk '{print $3}'\`; \
-     echo 'Now you can execute \"get-started.sh ${NEWAPP}\"';"
+     realm-cli import --yes --path ./realm-tutorial-backend; \
+     echo 'You can now execute get-started.sh <APP NAME>';"
