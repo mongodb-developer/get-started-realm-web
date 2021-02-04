e00cbc74c64cad45f2b3f4fa3f062fa0ff34351c
Wan Bachtiar <sindbach@gmail.com>
2021-02-04 11:55:57 +1100

    Updated helper scripts to utilise Docker name volume for faster IO operations

--- get-started.sh
+++ get-started.sh
@@ -7,8 +7,9 @@ then
 fi 
 docker run -it --rm \
     -p 3000:3000 \
+    -v get-started-realm:/cache \
     -v "$(pwd)":/workspace \
     -w /workspace/realm-web realm-web \
     "cd realm-tutorial-web; \
-    sed -i 's/export const APP_ID = \"[< >a-zA-Z0-9]\+\";/export const APP_ID = \"${APP_NAME}\";/g' src/App.js;
-    npm install; npm start"
+    sed -i 's/export const APP_ID = \"[< >a-zA-Z0-9]\+\";/export const APP_ID = \"${APP_NAME}\";/g' src/App.js; \
+    npm start"
