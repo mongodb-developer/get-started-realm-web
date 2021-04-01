ecccd2790e21c6f9c78ae8279a79b751be815bda
Wan Bachtiar <sindbach@gmail.com>
2021-04-01 16:46:33 +1100

    Updated with GCR

--- get-setup.sh
+++ get-setup.sh
@@ -12,28 +12,11 @@ then
 fi 
 
 CLUSTER_NAME=${3:-"Cluster0"}
-
-echo "Creating get-started-realm Docker volume ... "
-docker volume create get-started-realm 
-docker run --rm \
-    -v get-started-realm:/cache \
-    --user root:0 realm-web \
-    "mkdir /cache/realm-web; \
-    chown -R ubuntu:ubuntu /cache/realm-web;"
 echo "Setting up environment ..."
 docker run --rm \
-    -v get-started-realm:/cache \
     -v "$(pwd)":/workspace \
-    -w /workspace/realm-web realm-web \
-    "git clone --depth 1 https://github.com/sindbach/realm-tutorial-backend.git; \
-     git clone --depth 1 https://github.com/sindbach/realm-tutorial-web.git; \
-     mkdir /cache/realm-web/node_modules; \
-     ln -s /cache/realm-web/node_modules /workspace/realm-web/realm-tutorial-web/node_modules; \
-     cd /workspace/realm-web/realm-tutorial-web/; \
-     echo 'Performing npm install, this may take a while ...'
-     npm install --force; \
-     cd /workspace/realm-web; \
-     sed -i 's/\"clusterName\": \"[a-zA-Z0-9]\+\"/\"clusterName\": \"${CLUSTER_NAME}\"/g' realm-tutorial-backend/services/mongodb-atlas/config.json;
+    -w /home/ubuntu ghcr.io/mongodb-developer/get-started-realm-web:0.1 \
+    "sed -i 's/\"clusterName\": \"[a-zA-Z0-9]\+\"/\"clusterName\": \"${CLUSTER_NAME}\"/g' realm-tutorial-backend/services/mongodb-atlas/config.json;
      realm-cli login --api-key ${PUBLIC_KEY} --private-api-key ${PRIVATE_KEY}; \
-     realm-cli import --yes --path ./realm-tutorial-backend; \
-     echo 'You can now execute get-started.sh <APP NAME>';"
+     realm-cli import --yes --path ./realm-tutorial-backend && \
+     echo 'You can now execute get-started.sh <APP ID>';"
